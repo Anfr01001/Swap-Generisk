076eca46a5e76cf95874f1a6efe79e9e8212566f
André Franzén <anfr01001@utb.vaxjo.se>
2019-11-08 13:27:39 +0100

    Generisk swap metod är klar med test kod

--- SwapGenerisk/SwapGenerisk/Program.cs
+++ SwapGenerisk/SwapGenerisk/Program.cs
@@ -3,7 +3,44 @@
 namespace SwapGenerisk {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            int a = 10; int b = 20;
+            string c = "Hej"; string d = "då";
+            double e = 1.5; double f = 123.5;
+            bool g = true; bool h = false;
+
+
+
+            Console.WriteLine("Test kod där jag skickar in Int, string, double  & bool");
+            Console.WriteLine("Int a = {0} och int b = {1} blir", a, b);
+            new Swap<int>(ref a, ref b);
+            Console.WriteLine("Int a = {0} och int b = {1}", a, b);
+
+            Console.WriteLine("");
+
+            Console.WriteLine(" Och nu med strings String ");
+            Console.WriteLine("string c = {0} och string d = {1} som blir", c, d);
+            new Swap<string>(ref c, ref d);
+            Console.WriteLine("string c = {0} och string d = {1}", c, d);
+
+            Console.WriteLine("");
+
+            Console.WriteLine(" Och nu med doubles double ");
+            Console.WriteLine("double e = {0} och double f = {1} som blir", e, f);
+            new Swap<double>(ref e, ref f);
+            Console.WriteLine("double e = {0} och double f = {1}", e, f);
+
+            Console.WriteLine("");
+
+            Console.WriteLine(" Och slutligen med bool");
+            Console.WriteLine("bool g = {0} och bool h = {1} som blir", g, h);
+            new Swap<bool>(ref g, ref h);
+            Console.WriteLine("bool g = {0} och bool h = {1}", g, h);
+
+            Console.ReadLine(); // vänta
+
+
+
+
         }
     }
 }
---
+++ SwapGenerisk/SwapGenerisk/Swap.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SwapGenerisk {
+
+    class Swap<T> {
+
+        public  Swap(ref T thing1, ref T thing2) {
+            T temp = thing1;
+            thing1 = thing2;
+            thing2 = temp;
+        }
+    }
+}
